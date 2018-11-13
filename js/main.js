fb05bdcf3f2b90d43e5c16112734737756c7bf7d
anayarojo <raulmartin_04@hotmail.com>
2018-11-13 16:20:11 -0700

    Implement getScriptURL

--- js/main.js
+++ js/main.js
@@ -1,40 +1,49 @@
-var Main = (function(){
+var Main = (function () {
 
     let obj = {};
 
-    obj.run = function(fn, ...args){
-        getFunction(fn).then(function(func){
-            if(func && typeof func == 'function') func(args);
+    obj.run = function (fn, ...args) {
+        getFunction(fn).then(function (func) {
+            if (func && typeof func == 'function') func(args);
         });
     };
 
-    function getFunction(fn){
-        return new Promise(function(resolve, reject){
+    obj.getScriptURL = function () {
+        var scripts = document.getElementsByTagName('script');
+        var index = scripts.length - 1;
+        var myScript = scripts[index];
+        return function () {
+            return myScript.src;
+        };
+    };
+
+    function getFunction(fn) {
+        return new Promise(function (resolve, reject) {
             let name = `script${fn}`;
-            if(typeof obj[name] == 'undefined') {
+            if (typeof obj[name] == 'undefined') {
                 let url = `${window.location.origin}/js/others/script${fn}.js`;
-                loadScript(url, function(){
+                loadScript(url, function () {
                     console.info(`Load ─ ${url}`);
                     resolve(obj[name]);
                 });
             } else {
-                resolve(obj[name]);    
-            } 
+                resolve(obj[name]);
+            }
         });
     }
-    
+
     function loadScript(url, callback) {
         var script = document.createElement('script');
         if (script.readyState) { // Internet Explorer
             script.onreadystatechange = function () {
                 if (script.readyState === 'loaded' || script.readyState === 'complete') {
                     script.onreadystatechange = null;
-                    if(callback) callback();
+                    if (callback) callback();
                 }
             };
         } else { // Others
             script.onload = function () {
-                if(callback) callback();
+                if (callback) callback();
             };
         }
         script.src = url;
